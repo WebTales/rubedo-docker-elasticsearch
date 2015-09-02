0e9cfeea3e2720a5f40236863141e299c43d137d
amsatique <amsatique@users.noreply.github.com>
2015-09-02 15:59:39 +0200

    Remove supervisor, autoconf cluster

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,20 @@
 #!/bin/bash
 set -e
 
-if [ ! -z "${USERNAME}" ] && [ ! -z "${PWDSUPERVISOR}" ]
-then
-    sed -i 's#username = Username#username = '"$USERNAME"'#g' /etc/supervisord.conf
-    sed -i 's#password = PwdSupervisor#password = '"$PWDSUPERVISOR"'#g' /etc/supervisord.conf
+NODE_ID=${HOSTNAME: -1}
+CLUSTER_NAME=$(echo ${HOSTNAME} | cut -d '-' -f2)                                       
+#Install elasticsearch
+wget -O elasticsearch.tar.gz https://download.elasticsearch.org/elasticsearch/elasticsearch/"${ES_VERSION}".tar.gz && tar -zxvf elasticsearch.tar.gz -C /usr/share/elasticsearch --strip-components=1 && rm -f elasticsearch.tar.gz
+if [ "${PLUGINS}" != "**None**" ]; then
+		IFS=',' read -a array <<< "$PLUGINS"
+		for element in "${array[@]}"
+       		do
+            	/usr/share/elasticsearch/bin/plugin install elasticsearch/"$element"
+		done
 fi
+bin/plugin --install mobz/elasticsearch-head
+sed -i -e "s/#cluster.name: elasticsearch/cluster.name: $CLUSTER_NAME/g" config/elasticsearch.yml
+sed -i -e "s/#node.name: \"Franz Kafka\"/node.name: \"node_$NODE_ID\"/g" config/elasticsearch.yml
 
 exec "$@"
+
