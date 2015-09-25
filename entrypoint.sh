738cb34af70b0b3a0c324c3b3d6eb917baecfc77
amsatique <amsatique@MacBook-Pro-de-amsatique.local>
2015-09-25 13:29:32 +0200

    Add plugin verification

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,19 +2,20 @@
 set -e
 
 NODE_ID=${HOSTNAME: -1}
-CLUSTER_NAME=$(echo ${HOSTNAME} | cut -d '-' -f2)                                       
+CLUSTER_NAME=$(echo ${HOSTNAME} | cut -d '-' -f2)
 #Install elasticsearch
 wget -O elasticsearch.tar.gz https://download.elasticsearch.org/elasticsearch/elasticsearch/"${ES_VERSION}".tar.gz && tar -zxvf elasticsearch.tar.gz -C /usr/share/elasticsearch --strip-components=1 && rm -f elasticsearch.tar.gz
-if [ "${PLUGINS}" != "**None**" ]; then
-		IFS=',' read -a array <<< "$PLUGINS"
-		for element in "${array[@]}"
-       		do
-            	/usr/share/elasticsearch/bin/plugin install elasticsearch/"$element"
-		done
+#Install plugins
+if [ $(find /usr/share/elasticsearch/plugins/ -maxdepth 0 -type d -empty 2>/dev/null) ]; then
+	if [ "${PLUGINS}" != "**None**" ]; then
+			IFS=',' read -a array <<< "$PLUGINS"
+			for element in "${array[@]}"
+	       		do
+	            	/usr/share/elasticsearch/bin/plugin install elasticsearch/"$element"
+			done
+	fi
+	bin/plugin --install mobz/elasticsearch-head
+	sed -i -e "s/#cluster.name: elasticsearch/cluster.name: $CLUSTER_NAME/g" config/elasticsearch.yml
+	sed -i -e "s/#node.name: \"Franz Kafka\"/node.name: \"node_$NODE_ID\"/g" config/elasticsearch.yml
 fi
-bin/plugin --install mobz/elasticsearch-head
-sed -i -e "s/#cluster.name: elasticsearch/cluster.name: $CLUSTER_NAME/g" config/elasticsearch.yml
-sed -i -e "s/#node.name: \"Franz Kafka\"/node.name: \"node_$NODE_ID\"/g" config/elasticsearch.yml
-
 exec "$@"
-
